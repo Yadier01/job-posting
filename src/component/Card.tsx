60606a758be072f9d200203989e7b857a29738a6
Yadier <yadierg7@gmail.com>
2023-04-16 16:03:29 -0400

    added search filter if clicked on a lenguage etc

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -19,30 +19,44 @@ interface JobData {
 }
 
 function Card() {
-  const [userClick, setUserClick] = useState<string | null>(null);
+  const [userClicks, setUserClicks] = useState<string[]>([]);
 
-  const lenguagesHandler = useCallback(
+  const handleResetClick = useCallback(() => {
+    setUserClicks([]);
+  }, []);
+
+  const languagesHandler = useCallback(
     ({ currentTarget: { textContent } }: React.MouseEvent<HTMLElement>) => {
-      setUserClick(textContent);
+      setUserClicks((clicks: string[]) => [...clicks, textContent ?? ""]);
     },
     []
   );
 
+  // Remove a click from the list of user clicks
+  const removeUserClick = useCallback((clickToRemove: string) => {
+    setUserClicks((clicks) =>
+      clicks.filter((click) => click !== clickToRemove)
+    );
+  }, []);
+
   //render Jobs
   function renderJobs() {
     const jobs = jsonData as JobData[];
-    const filteredJobs = userClick
-      ? jobs.filter(
-          (job) =>
-            job.languages.includes(userClick) ||
-            (job.tools && job.tools.includes(userClick)) ||
-            job.role.includes(userClick) ||
-            job.level.includes(userClick)
-        )
-      : jobs;
+    const filteredJobs =
+      userClicks.length > 0
+        ? jobs.filter((job) =>
+            userClicks.every(
+              (click) =>
+                job.languages.includes(click) ||
+                (job.tools && job.tools.includes(click)) ||
+                job.role.includes(click) ||
+                job.level.includes(click)
+            )
+          )
+        : jobs;
 
     return filteredJobs.map((job) => (
-      <JobCard key={job.id} job={job} onClick={lenguagesHandler} />
+      <JobCard key={job.id} job={job} onClick={languagesHandler} />
     ));
   }
 
@@ -53,6 +67,47 @@ function Card() {
         alt=""
         className="w-full bg-darkCyan"
       />
+      {/* Show all selected language/tool/role/level and reset button */}
+      {userClicks.length > 0 && (
+        <div className="bg-black flex justify-between items-center">
+          <div>
+            {userClicks.map((click) => (
+              <span
+                key={click}
+                className=" text-lg py-2 px-4 mr-4 inline-flex items-center border rounded-full font-semibold"
+              >
+                {click}
+                <button
+                  type="button"
+                  onClick={() => removeUserClick(click)}
+                  className="ml-2 h-6 w-6 flex items-center justify-center rounded-full bg-red-600 hover:bg-red-700 focus:outline-none"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="black"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </svg>
+                </button>
+              </span>
+            ))}
+          </div>
+          <button
+            className="bg-red-600 hover:bg-red-700 text-black rounded px-4 py-2 mr-4"
+            onClick={handleResetClick}
+          >
+            Reset
+          </button>
+        </div>
+      )}
       <div className="bg-zinc-300 bg-bgC py-16">
         <div className="max-w-screen-xl mx-auto px-4 space-y-8">
           {renderJobs()}
